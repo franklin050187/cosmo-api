add0a31fe9f1f3a1c30317117676d2641220345f
franklin050187 <cat050187@gmail.com>
2023-09-01 23:20:36 +0200

    go

---
+++ api/server.py
@@ -0,0 +1,23 @@
+from api.shipcomcot import analyze_ship
+import json
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/analyze')
+def analyze():
+    url = request.args.get('url')
+    if not url:
+            # Create a dictionary with your values
+        data = {
+        "error": "No URL provided",
+        }
+        return json.dumps(data)
+    else:
+        return analyze_ship(url)
