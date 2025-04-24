83b418b233556845211bd5ceb3fead5472d41684
Johnn Kaykay <cat050187@gmail.com>
2025-04-24 15:21:18 +0200

    log

--- server.py
+++ server.py
@@ -698,7 +698,10 @@ async def insert_ship(data: ShipDataInsert = Body(...)):
 # post edit
 @app.get("/testdb")
 async def test_db():
-    return {"message":db_manager.debug_me(), "rows":db_manager.debug_me2()}
+    try :
+        return {"message":db_manager.debug_me(), "rows":db_manager.debug_me2()}
+    except Exception as e:
+        return {"error":e}
 
 
 
